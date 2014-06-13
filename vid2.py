dde6e6c2a0fe687fdb2fdd2e0ba712a2b99ea0d0
julzhk <jharley@vam.ac.uk>
2014-06-13 18:59:03 +0100

    test show other video properties

--- vid2.py
+++ vid2.py
@@ -10,5 +10,7 @@ while True:
         movie.rewind()
         movie.play()
         print(movie.get_frame())
+        print('has video ,',movie.has_video())
+        print('size ,',movie.get_size())
     if pygame.QUIT in [e.type for e in pygame.event.get()]:
         break
