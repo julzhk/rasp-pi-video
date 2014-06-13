999cc2733688c8590cb44ba7a01d1ba9e751664b
julzhk <jharley@vam.ac.uk>
2014-06-13 18:37:36 +0100

    add video

---
+++ vid2.py
@@ -0,0 +1,13 @@
+import pygame
+from time import sleep
+pygame.init()
+screen = pygame.display.set_mode((320,240))
+movie = pygame.movie.Movie("centaur_1.mpg")
+movie.play()
+while True:
+    if not(movie.get_busy()):
+        print("rewind")
+        movie.rewind()
+        movie.play()
+    if pygame.QUIT in [e.type for e in pygame.event.get()]:
+        break
