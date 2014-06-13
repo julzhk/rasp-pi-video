013eb46e02b496c1a305f98217fb2c0634640632
julzhk <jharley@vam.ac.uk>
2014-06-13 20:06:10 +0100

    read / write pin data

---
+++ button.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+# this reads a button(pin 24)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN)
+count = 0
+while True:
+    inputValue = GPIO.input(24)
+    if (inputValue == True):
+    count = count + 1
+    print("Button pressed " + str(count) + " times.")
+    time.sleep(.01)
