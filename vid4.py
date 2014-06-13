9761f954a5cda54af75573f8b08848faabd6ff76
julzhk <jharley@vam.ac.uk>
2014-06-13 19:35:33 +0100

    do pause with a already_paused = True flag

--- vid4.py
+++ vid4.py
@@ -4,14 +4,17 @@ pygame.init()
 screen = pygame.display.set_mode((320,240))
 movie = pygame.movie.Movie("centaur_1.mpg")
 movie.play()
+already_paused = False
 while True:
     print(movie.get_frame())
-    if movie.get_frame() == 200:
+    if movie.get_frame() > 200 and not already_paused :
         movie.pause()
         sleep(7)
+        already_paused = True
         movie.pause()
     if not(movie.get_busy()):
         print("rewind")
+        already_paused = False
         movie.rewind()
         movie.play()
         print(movie.get_frame())
