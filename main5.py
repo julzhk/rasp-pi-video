bd09b0a6649bb4e94023f79b7bd49fc630d2b942
julzhk <jharley@vam.ac.uk>
2014-07-07 11:35:31 +0100

    use threaded timer

--- main5.py
+++ main5.py
@@ -1,33 +1,7 @@
 import time
 import pifacedigitalio
 import threading
-#timer heartbeat in seconds
-TIMER_INTERVAL = 1.0
-class ledcontrol(threading.Thread):
-    def run(self):
-        for count in range(0, int(self.duration // self.interval)):
-            threading._sleep(self.interval)
-            print "Tick"
-            led_off(self.pin)
-
-
-    def __init__(self, pin, durat=5):
-        threading.Thread.__init__(self)
-        self.pin = pin
-        self.interval = TIMER_INTERVAL
-        self.duration = durat
-
-class glovecontrol(threading.Thread):
-    def run(self):
-        for count in range(0, int(self.duration // self.interval)):
-            threading._sleep(self.interval)
-            print "Tick"
-
-    def __init__(self, pin, durat=5):
-        threading.Thread.__init__(self)
-        self.interval = TIMER_INTERVAL
-        self.duration = durat
-
+from threaded_timer import timer_control
 pfd = pifacedigitalio.PiFaceDigital()
 import pygame
 
@@ -38,45 +12,49 @@ OFFPIN = 2
 STARTPIN = 3
 FPS = 60
 DEBUG = True
+
 def led_off(pin):
     pfd.leds[pin].turn_off()
-def led_on(pin,autooff=30):
+def led_on(pin):
     pfd.leds[pin].turn_on()
-    ledcontrol(pin=pin, durat= 6.0).start()
 def activate_glove():
-    pass
-    led_on(GLOVETESTPIN)
+        led_on(GLOVETESTPIN)
+        timer_control(funktion=led_off,args=GLOVETESTPIN).start()
 
 def glovetest():
     activate_glove()
-
 def off():
     raise
-    exit()
+
 def start():
     movie.play()
-    playmovie()
     led_on(STARTPIN)
+    timer_control(funktion=led_off,args=STARTPIN).start()
+
+def reset():
+    led_on(RESETPIN)
+    timer_control(funktion=led_off,args=RESETPIN).start()
+    movie.rewind()
+    start()
+
 def debug():
     for i in range(0, 8):
         print i, ' ', pfd.input_pins[i].value,
     print
-def startproject():
-    movie = pygame.movie.Movie('parkinsons.mpg')
-    pygame.mouse.set_visible(not HIDE_MOUSE)
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    print screen
-    pygame.display.init()
-    print movie.get_size()
-    movie_screen = pygame.Surface((600,500))
-    movie.set_display(movie_screen)
+
+
+def turn_off_leds():
+    [pfd.leds[i].turn_off() for i in range(0, 4)]
+
+
+def start_project():
     while True:
         try:
             time.sleep(.01)
             screen.blit(movie_screen, (0, 0))
             pygame.display.update()
             clock.tick(FPS)
-            frame = movie.get_frame()
+            print DEBUG & movie.get_frame()
             if DEBUG:
                 debug()
             if pfd.input_pins[RESETPIN].value:
@@ -88,17 +66,19 @@ def startproject():
             if pfd.input_pins[STARTPIN].value:
                 start()
         except:
-            [pfd.leds[i].turn_off() for i in range(0,4)]
+            turn_off_leds()
             exit()
-
-def reset():
-    led_on(RESETPIN)
-    start_project()
-
-
 
 pygame.init()
 clock = pygame.time.Clock()
+pygame.display.init()
+movie_screen = pygame.Surface((600,500))
+movie = pygame.movie.Movie('parkinsons.mpg')
+movie.set_display(movie_screen)
+pygame.mouse.set_visible(not HIDE_MOUSE)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+print screen
+print movie.get_size()
 
 start_project()
 
---
+++ threaded_timer.py
@@ -0,0 +1,24 @@
+import time
+import threading
+
+class timer_control(threading.Thread):
+    def run(self):
+        threading._sleep(self.sleeptime)
+        print "Do this once after %s seconds, then quit", self.sleeptime
+        if self.funktion:
+            self.funktion(*self.args)
+        exit()
+
+    def __init__(self,funktion=None, args = args, sleeptime=4):
+        self.funktion = funktion
+        self.args = args
+        self.sleeptime = sleeptime
+        threading.Thread.__init__(self)
+
+# t = timer_control(funktion=do_once)
+# t.start()
+# while True:
+#     print 'threads', threading.active_count(),
+#     time.sleep(1)
+#     print '.',
+
