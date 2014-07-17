5f41b8d7ecd9b7c7d574335ff593b9e5fa27cb20
julzhk <jharley@vam.ac.uk>
2014-07-17 19:03:45 +0100

    mpeg test

---
+++ mpegtest.py
@@ -0,0 +1,10 @@
+global movie
+import pygame
+pygame.init()
+movie = pygame.movie.Movie("take3e.mpeg")
+if movie.has_video():
+    screen = pygame.display.set_mode(movie.get_size())
+    movie_length = movie.get_length()
+    movie.set_volume(0.99)
+    movie.set_display(screen)
+    movie.play()
